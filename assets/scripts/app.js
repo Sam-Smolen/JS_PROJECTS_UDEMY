e576d461f02e29ba89bb1c0776c2742942a8ed24
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-07-03 18:49:27 -0700

    update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -25,6 +25,14 @@ function writeToLog(operationIdentifier, prevResult, operationNumber, newResult)
 };
 
 function calculateResult(calculationType) {
+    if (
+    calculationType !== 'ADD' &&
+    calculationType !== 'SUBTRACT' &&
+    calculationType !== 'MULTIPLY' && 
+    calculationType !== 'DIVIDE'
+    ) {
+        return;
+    }
     const enteredNumber = getUserNumberInput()
     const initialResult = currentResult;
     let mathOperator;
