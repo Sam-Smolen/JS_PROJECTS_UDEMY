8f4a4776d02831b6a4490d1b07aed7f02d29cf5c
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-09-03 18:16:49 -0700

    using bind()

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -56,23 +56,44 @@ function calculateResult(calculationType) {
     writeToLog(calculationType, initialResult, enteredNumber, currentResult);
 }
 
-function add() {
-    calculateResult('ADD');
-}
+// function add() {
+//     calculateResult('ADD');
+// }
 
-function subtract() {
-    calculateResult('SUBTRACT');
-}
+// function subtract() {
+//     calculateResult('SUBTRACT');
+// }
 
-function multiply() {
-    calculateResult('MULTIPLY');
-}
+// function multiply() {
+//     calculateResult('MULTIPLY');
+// }
 
-function divide() {
-    calculateResult('DIVIDE');
-}
+// function divide() {
+//     calculateResult('DIVIDE');
+// }
 
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
+function calculate(operation) {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    let operator;
+    if(operation === 'ADD') {
+        currentResult += enteredNumber;
+        operator = '+';
+    } else if (operation === 'SUBTRACT') {
+        currentResult -= enteredNumber;
+        operator = '-';
+    } else if (operation === 'MULTIPLY') {
+        currentResult *= enteredNumber;
+        operator = '*';
+    } else {
+        currentResult /= enteredNumber;
+        operator = '/';
+    }
+    createAndWriteOutput(operator, initialResult, enteredNumber);
+    writeToLog(operation, initialResult, enteredNumber, currentResult);
+};
+
+addBtn.addEventListener('click', calculate.bind(this, 'ADD'));
+subtractBtn.addEventListener('click', calculate.bind(this, 'SUBTRACT'));
+multiplyBtn.addEventListener('click', calculate.bind(this, 'MULTIPLY'));
+divideBtn.addEventListener('click', calculate.bind(this, 'DIVIDE'));
